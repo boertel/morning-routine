907707b1c4db73048a056b82e5c4fc62cf7a920d
boertel <ben@comediadesign.com>
2021-03-22 08:02:18 -0700

    use right url

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 const ListItem = ({ name, src }) => {
   return (
     <div className="rounded border-2 border-primary px-4 py-3 hover:bg-primary hover:bg-opacity-30 cursor-pointer">
-      <h4 className="">{name}</h4>
+      <h4 className="pb-4">{name}</h4>
       <div className="flex justify-center">
-      <iframe width="560" height="315" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
       </div>
   )
@@ -11,8 +11,8 @@ const ListItem = ({ name, src }) => {
 
 export default function Home() {
   const resources = [
-    {type: 'video', name: '8 mins abs workout', src: '//www.youtube.com/watch?v=W-9L0J_9qag'},
-    {type: 'video', name: 'Fresh Start', src: '//www.youtube.com/watch?v=Vry_VI76_Es'},
+    {type: 'video', name: '8 mins abs workout', src: '//www.youtube-nocookie.com/embed/v=W-9L0J_9qag'},
+    {type: 'video', name: 'Fresh Start', src: '//www.youtube-nocookie.com/embed/Vry_VI76_Es'},
   ]
   return (
     <div className="md:container mx-auto dark:bg-black mt-4">
