448764dae3f9d6b3edb76f432b4ce66f681178a0
boertel <ben@comediadesign.com>
2021-08-12 10:28:31 +0200

    add login/signup buttons

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import List from "components/List";
+import Link from "next/link";
 import { useEntries } from "resources/entry";
 import { useUser } from "resources/user";
 import { Header, PageTitle, Footer } from "ui";
@@ -10,8 +11,22 @@ export default function Home() {
     <>
       <PageTitle>Good Morning!</PageTitle>
       <Header />
-      <List data={data} />
+      {!user ? <LoggedOutButtons /> : <List data={data} />}
       <Footer />
     </>
+  );
+}
+
+function LoggedOutButtons() {
+  return (
+    <div className="text-center mt-20  flex-grow w-full">
+      <Link href="/auth/login">
+        <a>Login</a>
+      </Link>
+      <span className="text-gray-400">&nbsp;or&nbsp;</span>
+      <Link href="/auth/signup">
+        <a>Signup</a>
+      </Link>
+    </div>
   );
 }
